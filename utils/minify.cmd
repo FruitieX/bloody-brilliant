d98b8077bd779b9955f29a3bb14226fcb153b7c0
Henrik Hillner <hhillner@gmail.com>
2017-07-12 16:34:10 +0300

    Adds windows version of minify.sh (which gives parse errors when ran)

---
+++ utils/minify.cmd
@@ -0,0 +1,33 @@
+:: install dependencies:
+::
+:: npm install -g mishoo/UglifyJS2\::harmony
+:: or npm install -g uglify-js
+:: npm install -g glslmin
+:: npm install -g regpack
+
+rm -rf dist
+
+mkdir dist\temp
+cp mintemplate.html dist/temp/temp1.html
+cd dist
+
+:: js src
+::uglifyjs ..\src\song.js ..\src\player.js ..\src\index.js | node ..\utils\findandreplace.js --template temp\temp1.html --find '{{javascript}}' > temp\temp2.html
+:: crunch with regpack
+echo "uglifying..."
+uglifyjs ..\src\song.js ..\src\player.js ..\src\index.js -c  unsafe,unsafe_comps,unused,dead_code,drop_console,unsafe_math -m toplevel,eval  > temp\temp1.js
+
+:: vertex shader
+echo "minifying vertex shader..."
+glslmin ..\src\vertex.glsl | node ..\utils\findandreplace.js --template temp\temp1.js --find 'require(".\vertex.glsl")' --surround '`' > temp\temp2.js
+
+:: fragment shader
+echo "minifying fragment shader..."
+glslmin -m ..\src\fragment.glsl | node ..\utils\findandreplace.js --template temp\temp2.js --find 'require(".\fragment.glsl")' --surround '`' > temp\temp3.js
+
+echo "running regpack..."
+node ..\node_modules\.bin\regpack temp\temp3.js | node ..\utils\findandreplace.js --template temp\temp1.html --find '{{javascript}}' > temp\temp.html
+
+cp temp\temp.html index.html
+
+echo "wrote index.html ($(cat index.html | wc -c) bytes)"
