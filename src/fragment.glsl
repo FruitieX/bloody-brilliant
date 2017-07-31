be6875799d15af800f19de46039b30122031674b
Rasmus Eskola <fruitiex@gmail.com>
2017-07-31 19:13:25 +0300

    golf (+141b)

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -25,8 +25,8 @@ vec4 opU(vec4 d1, vec4 d2) {
 // Rotate around a coordinate axis (i.e. in a plane perpendicular to that axis) by angle <a>.
 // Read like this: R(p.xz, a) rotates "x towards z".
 // This is fast if <a> is a compile-time constant and slower (but still practical) if not.
-void pR(inout vec2 p, float a) {
-	p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
+vec2 pR(inout vec2 p, float a) {
+	return p = cos(a)*p + sin(a)*vec2(p.y, -p.x);
 }
 
 float fCapsule(vec3 p, float r, float c) {
